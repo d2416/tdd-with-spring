b438645c66b501a88e31d37a320aff23e9e2f057
David <david.pineda@campsited.com>
2021-08-19 10:59:49 +0100

    create customer repository and set entity

---
+++ src/main/java/com/amigoscode/testing/customer/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.amigoscode.testing.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+}
